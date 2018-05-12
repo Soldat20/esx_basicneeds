a7fe8e18af0539fad31690c5b7ed715dfbdbe33e
Soldat20 <chrismcwilliams89@gmail.com>
2018-05-12 18:55:31 -0400

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -15,6 +15,32 @@ ESX.RegisterUsableItem('bread', function(source)
 
 end)
 
+ESX.RegisterUsableItem('apple', function(source)
+
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+	xPlayer.removeInventoryItem('apple', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_apple'))
+
+end)
+
+ESX.RegisterUsableItem('chips', function(source)
+
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+	xPlayer.removeInventoryItem('chips', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'hunger', 200000)
+	TriggerClientEvent('esx_basicneeds:onEat', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_chips'))
+
+end)
+
 ESX.RegisterUsableItem('water', function(source)
 
 	local _source = source
@@ -25,5 +51,18 @@ ESX.RegisterUsableItem('water', function(source)
 	TriggerClientEvent('esx_status:add', source, 'thirst', 200000)
 	TriggerClientEvent('esx_basicneeds:onDrink', source)
 	TriggerClientEvent('esx:showNotification', source, _U('used_water'))
+
+end)
+
+ESX.RegisterUsableItem('pop', function(source)
+
+	local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+	xPlayer.removeInventoryItem('pop', 1)
+
+	TriggerClientEvent('esx_status:add', source, 'thirst', 200000)
+	TriggerClientEvent('esx_basicneeds:onDrink', source)
+	TriggerClientEvent('esx:showNotification', source, _U('used_pop'))
 
 end)
