23b892214770dc89a3884572be9e081a1bb2bfc8
harlekin315 <39226885+harlekin315@users.noreply.github.com>
2018-05-12 20:09:12 -0400

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,22 @@
 Locales['en'] = {
 
-	['used_bread'] = 'you have used 1x bread',
+	['used_roll'] = 'you have used 1x roll',
+	['used_chips'] = 'you have used 1x chips',
+	['used_fruit'] = 'you have used 1x fruit',
+	['used_burger'] = 'you have used 1x burger',
+	['used_hot dog'] = 'you have used 1x hot dog',
+	['used_ribs'] = 'you have used 1x ribs',
+	['used_pancakes'] = 'you have used 1x pancakes',
+	['used_grilled cheese'] = 'you have used 1x grilled cheese',
+	['used_cookie'] = 'you have used 1x cookie',
+	['used_candy bar'] = 'you have used 1x candy bar',
+	['used_mac n cheese'] = 'you have used 1x mac n cheese',
+	['used_pizza'] = 'you have used 1x pizza',
 	['used_water'] = 'you have used 1x water',
+	['used_pop'] = 'you have used 1x pop',
+	['used_monster'] = 'you have used 1x monster',
+	['used_red bull'] = 'you have used 1x red bull',
+	['used_milk'] = 'you have used 1x milk',
+	['used_coffee'] = 'you have used 1x coffee',
 
 }
