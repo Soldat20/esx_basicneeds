086911217dfad4b1ee67ef430d0104e9f3a38bac
harlekin315 <39226885+harlekin315@users.noreply.github.com>
2018-05-13 13:29:23 -0400

    Update esx_basicneeds.sql

--- esx_basicneeds.sql
+++ esx_basicneeds.sql
@@ -1,9 +1,23 @@
 USE `essentialmode`;
 
 INSERT INTO `items` (name, label) VALUES 
-	('bread', 'Bread'),
+	('roll', 'Roll'),
 	('chips', 'Chips'),
-	('apple', 'Apple'),	
+	('fruit', 'Fruit'),
+	('burger', 'Burger'),
+	('hot dog', 'Hot Dog'),
+	('ribs', 'Ribs'),
+	('pancakes', 'Pancakes'),
+	('grilled cheese', 'Grilled Cheese'),
+	('cookie', 'Cookie'),
+	('candy bar', 'Candy Bar'),
+	('mac n cheese', 'Mac n Cheese'),
+	('pizza', 'Pizza'),
 	('pop', 'Pop'),
 	('water', 'Water')
+	('monster', 'Monster'),
+	('red bull', 'Red Bull'),
+	('coffee', 'Coffee'),
+	('milk', 'Milk'),
+	
 ;
